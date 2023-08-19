55942b86cb729836b2a99231be0f0637bb3c4e29
Daniel Thornton <dshaggy28@yahoo.com>
2023-08-19 15:37:39 -0700

    Putting all files together

---
+++ Actor.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+class_name Actor
+
+
+var speed = Vector2(800.0, 1000)
+var gravity = 3000.0
+var velocity1 = Vector2.ZERO
+
+
+
+	
+	
+
+
+	
+
+	
---
+++ src/Actors/player.gd
@@ -0,0 +1,25 @@
+extends Actor
+
+func _physics_process(delta):
+	var direction = get_direction()
+	velocity1 = speed * direction
+	set_velocity(velocity1)
+	move_and_slide()
+	
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	  ) 
+	
+func calculate_move_velocity(linear_velocity,
+							  direction,
+							 speed,
+							  delta):
+	var new_velocity = linear_velocity
+	new_velocity = speed.x * direction.x
+	velocity.y += gravity * delta
+	if direction.y == -1.0:
+		velocity.y = speed.y * direction.y
+	return speed * direction
+
